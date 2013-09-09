6af75276db6d831ebbc4f473f59d1837be09abbb
Jean-Christophe GAY <contact@jeanchristophegay.com>
2013-09-09 21:19:22 +0200

    P07: Flatten a nested list structure.

---
+++ src/p07.scala
@@ -0,0 +1,11 @@
+
+def flatten(value: List[Any]): List[Any] = value flatMap {
+  element => element match {
+    case list: List[_] => flatten(list)
+    case singleElement => List(element)
+  }
+}
+
+assert(flatten(List(List(1, 1), 2, List(3, List(5, 8)))) == List(1, 1, 2, 3, 5, 8))
+
+println("OK 1")
