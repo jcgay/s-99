585f13e2737303714e398cddba0fcfb08f1a8227
Jean-Christophe GAY <contact@jeanchristophegay.com>
2013-09-11 19:22:49 +0200

    P10: Run-length encoding of a list.

---
+++ src/p10.scala
@@ -0,0 +1,12 @@
+
+def pack(symbols: List[Symbol]): List[List[Symbol]] = {
+  val result: (List[Symbol], List[Symbol]) = symbols span { _ == symbols.head }
+  if (result._2 == Nil) List(result._1) else result._1 :: pack(result._2)
+}
+
+def encode(symbols: List[Symbol]) = pack(symbols) map { a =>  (a.length, a.head)}
+
+assert(encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+  == List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e)))
+
+println("OK")
