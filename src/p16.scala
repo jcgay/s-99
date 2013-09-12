94d774c76b3d1e17418dace9cb169c165aa4351a
Jean-Christophe GAY <contact@jeanchristophegay.com>
2013-09-12 21:46:58 +0200

    P16: Drop every Nth element from a list.

---
+++ src/p16.scala
@@ -0,0 +1,14 @@
+
+def drop(i: Int, symbols: List[Symbol]) = {
+  def dropRecursive(count: Int, elements: List[Symbol]): List[Symbol] = (count, elements) match {
+    case (_, Nil) => Nil
+    case (1, head :: tail) => head :: dropRecursive(count + 1, tail)
+    case (_, head :: tail) if (count % i == 0) => dropRecursive(count + 1, tail)
+    case (_, head :: tail) => head :: dropRecursive(count + 1, tail)
+  }
+  dropRecursive(1, symbols)
+}
+
+assert(drop(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) == List('a, 'b, 'd, 'e, 'g, 'h, 'j, 'k))
+
+println("OK")
