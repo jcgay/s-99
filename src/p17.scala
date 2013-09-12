34d60ac6c0daaa9227a3da38d18e97aca597b654
Jean-Christophe GAY <contact@jeanchristophegay.com>
2013-09-12 22:22:51 +0200

    P17: Split a list into two parts.

---
+++ src/p17.scala
@@ -0,0 +1,21 @@
+
+def split(i: Int, symbols: List[Symbol]) = symbols.splitAt(i)
+
+assert(split(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+  == (List('a, 'b, 'c),List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k)))
+
+println("OK 1")
+
+def splitRecursive(i: Int, symbols: List[Symbol]): (List[Symbol], List[Symbol]) = (i, symbols) match {
+    case (_, Nil) => (Nil, Nil)
+    case (0, values) => (List(), values)
+    case (_, head :: tail) => {
+      val (before, after) = splitRecursive(i - 1, tail)
+      (head :: before, after)
+    }
+}
+
+assert(splitRecursive(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+  == (List('a, 'b, 'c),List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k)))
+
+println("OK 2")
