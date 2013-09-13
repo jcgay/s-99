8a39172e405b99654fd899942b125cd7d7caf073
Jean-Christophe GAY <contact@jeanchristophegay.com>
2013-09-13 23:14:12 +0200

    P18: Extract a slice from a list.

--- src/main/scala/P18.scala
+++ src/main/scala/P18.scala
@@ -2,4 +2,13 @@
 object P18 {
 
   def slice[A](start: Int, end: Int, elements: List[A]) = elements.slice(start, end)
+
+  def sliceRecursive[A](start: Int, end: Int, elements: List[A]) = {
+    def slicer(count:Int, values: List[A]): List[A] = {
+      if (count >= start && count < end) values.head :: slicer(count + 1, values.tail)
+      else if (count == end) Nil
+      else slicer(count + 1, values.tail)
+    }
+    slicer(0, elements)
+  }
 }
--- src/test/scala/P18Spec.scala
+++ src/test/scala/P18Spec.scala
@@ -4,4 +4,8 @@ class P18Spec extends UnitSpec {
   it should "slice" in {
     P18.slice(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) should equal (List('d, 'e, 'f, 'g))
   }
+
+  it should "slice list using recursion" in {
+    P18.sliceRecursive(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) should equal (List('d, 'e, 'f, 'g))
+  }
 }
