3bf122af570b108bfeedfb0d33fe0a96ed1edf30
Jean-Christophe GAY <contact@jeanchristophegay.com>
2013-09-13 22:21:49 +0200

    Pattern matching can be used with flatMap.

--- src/main/scala/p07.scala
+++ src/main/scala/p07.scala
@@ -2,9 +2,7 @@
 object P07 {
 
   def flatten(value: List[Any]): List[Any] = value flatMap {
-    element => element match {
-      case list: List[_] => flatten(list)
-      case singleElement => List(element)
-    }
+    case list: List[_] => flatten(list)
+    case singleElement => List(singleElement)
   }
 }
