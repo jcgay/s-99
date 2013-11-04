4a2ece6c7f9b32fe483e5cea89d78130ab1f2dd2
Jean-Christophe GAY <contact@jeanchristophegay.com>
2013-11-04 22:23:39 +0100

    Add tests for P34.

--- src/test/scala/P34Spec.scala
+++ src/test/scala/P34Spec.scala
@@ -4,6 +4,15 @@ import arithmetic.S99Int._
 class P34Spec extends FlatSpec {
 
   it should "calculate Euler's totient function phi(m)" in {
+    assert(1.totient == 1)
+    assert(2.totient == 1)
+    assert(3.totient == 2)
+    assert(4.totient == 2)
+    assert(5.totient == 4)
+    assert(6.totient == 2)
+    assert(7.totient == 6)
+    assert(8.totient == 4)
+    assert(9.totient == 6)
     assert(10.totient == 4)
   }
 }
